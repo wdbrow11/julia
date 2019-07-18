b61d3a5d385b6c4b17e7181fbab5b8c3f7ab1706
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:53:53 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var shoutouts = [
+  "Your perseverance and determination when coding :) plus i love your music + tv show taste",
+  "you are super hardworking. I love your shirt!",
+  "how kind and inclusive you are to everyone and how easy you are to talk to. It made this camp a lot better, especially on the first day.",
+  "You're super cool! I like your quiet presence and how you like the same artists that I do.",
+  "How you treat everyone so respectfully!",
+  "You're very nice!",
+  "your Junior Jewels shirt! I seriously thought you purchased it off a fansite and just added "Redkode City" with a sharpie."
+  "that you put in tons of effort with whatever you do",
+  "Your taste in music and shows!"
+  "Your easy-going attitude in both problem solving situations and in general. I love seeing you in the morning and talking with you about random things. You always make conversation interesting and all your ideas are really creative.",
+  "your patience!",
+  "how kind you are and how wonderful talking to you is. You are so nice and I love how you have such a nice gentle touch and finnesse. Thanks for making camp so much more enjoyable. I also love how you know so much about a lot of TV shows and movies.",
+  "Your passion for everything you do! You are also so motivating and I don’t think I could work on the project without you."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
